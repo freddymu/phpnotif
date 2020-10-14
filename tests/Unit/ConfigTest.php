d90f9b9c01dbb3a4613efc580248907c61e2bde6
Freddy <fredhopelane@gmail.com>
2020-10-14 11:45:23 +0700

    add more test case

--- tests/Unit/ConfigTest.php
+++ tests/Unit/ConfigTest.php
@@ -5,6 +5,7 @@ namespace Freddymu\Phpnotif\Tests\Unit;
 use Freddymu\Phpnotif\Helper\Config;
 use Freddymu\Phpnotif\Tests\BaseTestCase;
 
+
 class ConfigTest extends BaseTestCase
 {
     /**
@@ -35,4 +36,19 @@ class ConfigTest extends BaseTestCase
         self::assertIsString($configContent);
         self::assertNotEmpty($configContent);
     }
+
+    /**
+     * @test
+     */
+    public function read_a_sub_key_using_numeric_value()
+    {
+        // Given
+        $configs = Config::get('connection.mongodb.1');
+
+        // When
+
+        // Then
+        self::assertEquals('', $configs);
+    }
+
 }
