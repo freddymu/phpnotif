4df563ac8f08006f0850af8770747f48a500c906
Freddy <fredhopelane@gmail.com>
2020-10-23 17:26:41 +0700

    add mock/stub example test case

--- tests/Unit/PhpNotifModelTest.php
+++ tests/Unit/PhpNotifModelTest.php
@@ -2,9 +2,63 @@
 
 namespace Freddymu\Phpnotif\Tests\Unit;
 
+use Freddymu\Phpnotif\Entities\PhpNotifEntity;
+use Freddymu\Phpnotif\Helper\Test;
 use Freddymu\Phpnotif\Tests\BaseTestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 class PhpNotifModelTest extends BaseTestCase
 {
-    //TODO: try to implement test doubles using mockery
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * This is a bad example of mocking/stub usage
+     * Because the system under test is insignificant or unclear
+     * I've put this here on purpose for my internal workshop
+     * @test
+     */
+    public function save()
+    {
+        // Given
+        $entity = Test::createEntity();
+
+        $sampleReturn = new \StdClass;
+        $sampleReturn->n = 1;
+        $mock = \Mockery::mock('Freddymu\Phpnotif\Models\PhpNotifModel');
+        $mock
+            ->shouldReceive('save')
+            ->andReturn([
+                $sampleReturn
+            ]);
+
+        // When
+        $result = $mock->save($entity->toArray());
+
+        // Then
+        self::assertEquals(1, $result[0]->n);
+    }
+
+    /**
+     * @test
+     */
+    public function getInboxByUserId()
+    {
+        self::markTestSkipped();
+    }
+
+    /**
+     * @test
+     */
+    public function setMessageAsRead()
+    {
+        self::markTestSkipped();
+    }
+
+    /**
+     * @test
+     */
+    public function getInboxSummary()
+    {
+        self::markTestSkipped();
+    }
 }
